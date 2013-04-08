bce7c3168aeb9f9c4d8a241983c1d13972d0fd1f
hustlzp <hustlzp@qq.com>
2013-04-08 15:53:49 +0800

    use jquery.cookie.js to handle cookie

--- xichuangzhu/static/js/s2t.js
+++ xichuangzhu/static/js/s2t.js
@@ -87,36 +87,3 @@ function Simplized(cc){
     }
     return str;
 }
-
-function setCookie(name, value)     //cookies设置
-{
-    var argv = setCookie.arguments;
-    var argc = setCookie.arguments.length;
-    var expires = (argc > 2) ? argv[2] : null;
-    if(expires!=null)
-    {
-        var LargeExpDate = new Date ();
-        LargeExpDate.setTime(LargeExpDate.getTime() + (expires*1000*3600*24));
-    }
-    document.cookie = 
-        name + "=" + escape (value) 
-        + ((expires == null) ? "" : ("; expires=" +LargeExpDate.toGMTString()))
-        + "path=/";
-}
-
-function getCookie(Name)            //cookies读取
-{
-    var search = Name + "="
-    if(document.cookie.length > 0) 
-    {
-        offset = document.cookie.indexOf(search)
-        if(offset != -1) 
-        {
-            offset += search.length
-            end = document.cookie.indexOf(";", offset)
-            if(end == -1) end = document.cookie.length
-            return unescape(document.cookie.substring(offset, end))
-		}
-		else return ""
-    }
-}
