b91735ac6608258a43b598ebdca7955842c7f171
hustlzp <hustlzp@qq.com>
2014-01-06 11:53:57 +0800

    format less files, and recompile less to min-size css, upgrade jquery.cookie to 1.4.0

--- xichuangzhu/__init__.py
+++ xichuangzhu/__init__.py
@@ -34,7 +34,7 @@ def inject_vars():
 # url generator for pagination
 def url_for_other_page(page):
     view_args = request.view_args.copy()
-    args = request.args.copy()
+    args = request.args.copy().to_dict()
     args['page'] = page
     view_args.update(args)
     return url_for(request.endpoint, **view_args)
@@ -49,12 +49,23 @@ def before_request():
     g.user_id = session['user_id'] if 'user_id' in session else None
 
 
-import log
+def register_logger(app):
+    """Send error log to admin by smtp"""
+    if not app.debug:
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = (config.SMTP_USER, config.SMTP_PASSWORD)
+        mail_handler = SMTPHandler((config.SMTP_SERVER, config.SMTP_PORT), config.SMTP_FROM, config.SMTP_ADMIN, 'xcz-log',
+                                   credentials)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 import models
 
 
 def register_routes(app):
     from .controllers import account, admin, author, dynasty, topic, site, user, work
+
     app.register_blueprint(site.bp, url_prefix='')
     app.register_blueprint(account.bp, url_prefix='/account')
     app.register_blueprint(admin.bp, url_prefix='/admin')
@@ -70,14 +81,16 @@ def register_error_handle(app):
     def page_403(error):
         return render_template('site/403.html'), 403
 
-    # @app.errorhandler(404)
-    # def page_404(error):
-    #     return render_template('site/404.html'), 404
+    @app.errorhandler(404)
+    def page_404(error):
+        return render_template('site/404.html'), 404
 
     @app.errorhandler(500)
     def page_500(error):
         return render_template('site/500.html'), 500
 
+
 register_routes(app)
 register_error_handle(app)
+register_logger(app)
 
