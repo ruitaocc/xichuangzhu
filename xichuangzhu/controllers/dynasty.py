a8a0c85cdf361dfa311ef7f591de61fa425c3397
hustlzp <hustlzp@qq.com>
2014-01-15 22:25:59 +0800

    finish refactor codes in add & edit dyansty

--- xichuangzhu/controllers/dynasty.py
+++ xichuangzhu/controllers/dynasty.py
@@ -1,49 +1,45 @@
 # coding: utf-8
-from flask import render_template, request, redirect, url_for, Blueprint
+from flask import render_template, redirect, url_for, Blueprint
 from ..models import db, Dynasty
 from ..utils import require_admin
+from ..forms import DynastyForm
 
 bp = Blueprint('dynasty', __name__)
 
 
 @bp.route('/<dynasty_abbr>')
 def view(dynasty_abbr):
-    """朝代信息"""
+    """朝代"""
     dynasties = Dynasty.query.order_by(Dynasty.start_year)
     dynasty = Dynasty.query.filter(Dynasty.abbr == dynasty_abbr).first_or_404()
     authors = dynasty.authors.order_by(db.func.rand()).limit(5)
     authors_num = dynasty.authors.count()
-    return render_template('dynasty/dynasty.html', dynasty=dynasty, authors=authors, authors_num=authors_num,
-                           dynasties=dynasties)
+    return render_template('dynasty/dynasty.html', dynasty=dynasty, authors=authors,
+                           authors_num=authors_num, dynasties=dynasties)
 
 
 @bp.route('/add', methods=['GET', 'POST'])
 @require_admin
 def add():
     """添加朝代"""
-    if request.method == 'GET':
-        return render_template('dynasty/add.html')
-    dynasty = Dynasty(name=request.form['name'], abbr=request.form['abbr'], intro=request.form['intro'],
-                      start_year=int(request.form['start_year']), end_year=int(request.form['end_year']))
-    db.session.add(dynasty)
-    db.session.commit()
-    return redirect(url_for('.view', dynasty_abbr=dynasty.abbr))
+    form = DynastyForm()
+    if form.validate_on_submit():
+        dynasty = Dynasty(**form.data)
+        db.session.add(dynasty)
+        db.session.commit()
+        return redirect(url_for('.view', dynasty_abbr=dynasty.abbr))
+    return render_template('dynasty/add.html', form=form)
 
 
 @bp.route('/<int:dynasty_id>/edit', methods=['GET', 'POST'])
 @require_admin
 def edit(dynasty_id):
-    """编辑朝代信息"""
-    if request.method == 'GET':
-        dynasty = Dynasty.query.get_or_404(dynasty_id)
-        return render_template('dynasty/edit.html', dynasty=dynasty)
-    else:
-        dynasty = Dynasty.query.get_or_404(dynasty_id)
-        dynasty.name = request.form['name']
-        dynasty.abbr = request.form['abbr']
-        dynasty.intro = request.form['intro']
-        dynasty.start_year = int(request.form['start_year'])
-        dynasty.end_year = int(request.form['end_year'])
+    """编辑朝代"""
+    dynasty = Dynasty.query.get_or_404(dynasty_id)
+    form = DynastyForm(obj=dynasty)
+    if form.validate_on_submit():
+        form.populate_obj(dynasty)
         db.session.add(dynasty)
         db.session.commit()
         return redirect(url_for('.view', dynasty_abbr=dynasty.abbr))
+    return render_template('dynasty/edit.html', dynasty=dynasty, form=form)
--- xichuangzhu/forms/admin.py
+++ xichuangzhu/forms/admin.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 from flask_wtf import Form
-from wtforms import TextField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email
+from wtforms import TextField, TextAreaField, SelectField, IntegerField
+from wtforms.validators import DataRequired
 
 
 class WorkForm(Form):
-    """Form for add and edit work"""
+    """Form for add & edit work"""
     title = TextField('标题', [DataRequired('标题不能为空')])
     type_id = SelectField('类别', [DataRequired("类别不能为空")], coerce=int)
     author_id = SelectField('作者', [DataRequired('作者不能为空')], coerce=int)
@@ -15,9 +15,19 @@ class WorkForm(Form):
 
 
 class AuthorForm(Form):
+    """Form for add & edit author"""
     name = TextField('姓名', [DataRequired('姓名不能为空')])
-    abbr = TextField('拼音缩写', [DataRequired('拼音缩写不能为空')])
+    abbr = TextField('拼音', [DataRequired('拼音不能为空')])
     dynasty_id = SelectField('朝代', [DataRequired('朝代不能为空')], coerce=int)
     birth_year = TextField('生年', [DataRequired('生年不能为空')])
     death_year = TextField('卒年')
     intro = TextAreaField('简介', [DataRequired('简介不能为空')])
+
+
+class DynastyForm(Form):
+    """Form for add & edit dynasty"""
+    name = TextField('朝代', [DataRequired('朝代不能为空')])
+    abbr = TextField('拼音', [DataRequired('拼音不能为空')])
+    intro = TextAreaField('简介', [DataRequired('简介不能为空')])
+    start_year = IntegerField('起始年', [DataRequired('起始年不能为空')])
+    end_year = IntegerField('结束年', [DataRequired('结束年不能为空')])
