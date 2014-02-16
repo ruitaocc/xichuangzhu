976d08a6143b318a5cf21ef52f8278985d2fe544
hustlzp <hustlzp@qq.com>
2014-02-16 09:47:42 +0800

    add required packages version, update sql data

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 from flask.ext.script import Manager
+from fabric.api import run as fabrun, env
 from xichuangzhu import app
 from xichuangzhu.models import db
 
@@ -7,12 +9,21 @@ manager = Manager(app)
 
 @manager.command
 def run():
+    """启动app"""
     app.run(debug=True)
 
 
 @manager.command
 def syncdb():
+    """根据model创建数据库tables"""
     db.create_all()
+
+
+@manager.command
+def backdb():
+    """将数据库中的表结构和数据提取为sql文件"""
+    env.host_string = "localhost"
+    fabrun("mysqldump -uroot -p xcz > /var/www/xichuangzhu/xcz.sql")
 
 
 @manager.command
