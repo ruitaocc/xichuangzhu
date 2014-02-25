8f66131559e121bf00fc864d729ff1bce1d361f7
hustlzp <hustlzp@qq.com>
2014-02-25 21:32:45 +0800

    begin to use bower!

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ def deploy():
     env.host_string = config.HOST_STRING
     with cd('/var/www/xichuangzhu'):
         run('git pull')
+        run('bower install')
         with prefix('source venv/bin/activate'):
             run('pip install -r requirements.txt')
         run('supervisorctl restart xcz')
