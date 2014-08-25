3df99710182fbaa275bb03ffef39acba10e55a86
Zhipeng Liu <hustlzp@qq.com>
2014-08-25 11:34:56 +0800

    Update default.py

--- config/default.py
+++ config/default.py
@@ -53,8 +53,7 @@ class Config(object):
     DOUBAN_SECRET = '4c87a8ef33e6c6be'
     DOUBAN_REDIRECT_URI = '%s/account/signin' % SITE_DOMAIN
     DOUBAN_LOGIN_URL = "https://www.douban.com/service/auth2/auth?client_id=%s&redirect_uri=%s" \
-                       "&response_type=code" % (
-        DOUBAN_CLIENT_ID, DOUBAN_REDIRECT_URI)
+                       "&response_type=code" % (DOUBAN_CLIENT_ID, DOUBAN_REDIRECT_URI)
 
     # Aliyun OSS config
     OSS_HOST = 'oss.aliyuncs.com'
