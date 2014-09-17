6b2b1ed59a0e974f85680c3feb61a09d36991849
hustlzp <hustlzp@qq.com>
2014-09-17 18:07:39 +0800

    Add codes for benchmark vs Node.js.

--- xichuangzhu/controllers/site.py
+++ xichuangzhu/controllers/site.py
@@ -10,7 +10,7 @@ def index():
     """首页"""
     works = Work.query.order_by(db.func.random()).limit(4)
     work_images = WorkImage.query.order_by(WorkImage.create_time.desc()).limit(18)
-    work_reviews = WorkReview.query.filter(WorkReview.is_publish == True).order_by(
+    work_reviews = WorkReview.query.filter(WorkReview.is_publish).order_by(
         WorkReview.create_time.desc()).limit(4)
     authors = Author.query.order_by(db.func.random()).limit(5)
     dynasties = Dynasty.query.order_by(Dynasty.start_year.asc())
@@ -30,7 +30,23 @@ def about():
     """关于页"""
     return render_template('site/about.html')
 
+
 @bp.route('/disclaimer')
 def disclaimer():
     """免责声明"""
     return render_template('site/disclaimer.html')
+
+
+@bp.route('/update')
+def update():
+    """Just for benchmark vs Node.js"""
+    import json
+    from ..models import Work
+
+    result = []
+    for work in Work.query.filter(Work.highlight):
+        result.append({
+            'id': work.id,
+            'update_at': work.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+        })
+    return json.dumps(result)
