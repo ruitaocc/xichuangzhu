0f95b50a67bbfcb8c2ed143919bcdb6ea7ef8134
hustlzp <hustlzp@qq.com>
2015-08-04 19:41:38 +0800

    Update timesince filter.

--- xichuangzhu/filters.py
+++ xichuangzhu/filters.py
@@ -10,17 +10,21 @@ from models import CollectWork, CollectWorkImage
 def timesince(value):
     """Friendly time gap"""
     now = datetime.datetime.now()
+
+    if now < value:
+        return "刚刚"
+
     delta = now - value
     if delta.days > 365:
-        return '%d年前' % (delta.days / 365)
+        return '%d 年前' % (delta.days / 365)
     if delta.days > 30:
-        return '%d个月前' % (delta.days / 30)
+        return '%d 个月前' % (delta.days / 30)
     if delta.days > 0:
-        return '%d天前' % delta.days
+        return '%d 天前' % delta.days
     if delta.seconds > 3600:
-        return '%d小时前' % (delta.seconds / 3600)
+        return '%d 小时前' % (delta.seconds / 3600)
     if delta.seconds > 60:
-        return '%d分钟前' % (delta.seconds / 60)
+        return '%d 分钟前' % (delta.seconds / 60)
     return '刚刚'
 
 
