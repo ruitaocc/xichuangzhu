0952eb753f9a9366ae611ff6db37709a4f5f0608
hustlzp <hustlzp@qq.com>
2015-08-12 21:26:01 +0800

    Add api.get_random_work.

--- xichuangzhu/controllers/api.py
+++ xichuangzhu/controllers/api.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from flask import render_template, Blueprint, request, url_for
-from ..models import db, Quote
+from jinja2 import Markup
+from ..models import db, Quote, Work, WorkType
 from ..utils import jsonify, absolute_url_for
+from ..filters import markdown_work, markdown
 
 bp = Blueprint('api', __name__)
 
@@ -15,4 +17,22 @@ def get_random_quote():
         'author': quote.author.name,
         'work': quote.work.title,
         'url': absolute_url_for('work.view', work_id=quote.work_id)
+    }
+
+
+@bp.route('/get_random_work')
+@jsonify
+def get_random_work():
+    work = Work.query.filter(Work.type.has(WorkType.en.in_(['shi', 'ci']))). \
+        order_by(db.func.random()).first()
+    content = work.content if not work.mobile_content else work.mobile_content
+    return {
+        'id': work.id,
+        'title': work.title,
+        'content': Markup(markdown_work(content)),
+        'intro': Markup(markdown(work.intro)) if work.intro else "",
+        'author': work.author.name,
+        'author_id': work.author_id,
+        'dynasty': work.author.dynasty.name,
+        'dynasty_id': work.author.dynasty_id
     }
