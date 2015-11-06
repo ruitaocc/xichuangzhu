c013fbe0d40e3decd3ca412a3f0031abe8e4f216
hustlzp <hustlzp@qq.com>
2015-11-06 17:58:09 +0800

    Continue refactor.

--- application/pages/work/edit/edit.js
+++ application/pages/work/edit/edit.js
@@ -2,6 +2,6 @@
     "use strict";
 
     $('.btn-search-wiki').click(function () {
-        window.open("http://baike.baidu.com/search?word={{ work.title }}{% if work.title_suffix %}{{ work.title_suffix }}{% endif %}");
+        window.open("http://baike.baidu.com/search?word=" + g.searchKeyWord);
     });
 })();
