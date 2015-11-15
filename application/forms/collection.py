cab0d3f02441d4b1b33143ebace139b026c3089f
hustlzp <hustlzp@qq.com>
2015-11-15 18:30:12 +0800

    Update collection form.

--- application/forms/collection.py
+++ application/forms/collection.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Optional
 class CollectionForm(Form):
     name = StringField('名称', validators=[DataRequired('集合名称不能为空')])
     full_name = StringField('全称', validators=[Optional()])
-    abbr = StringField('缩写', validators=[DataRequired('缩写不能为空')])
+    # abbr = StringField('缩写', validators=[DataRequired('缩写不能为空')])
     desc = TextAreaField('简介', validators=[Optional()])
     cover = StringField('封面', validators=[Optional()])
     link = StringField('链接', validators=[Optional()])
