3ac6894a672767b77ea1a2b668f68c6984cf9fa5
hustlzp <hustlzp@qq.com>
2016-02-10 20:16:24 +0800

    Fix typo.

--- application/utils/mobile.py
+++ application/utils/mobile.py
@@ -56,7 +56,7 @@ def generate_main_db():
         _work.content = _get_work_content(work)
         _work.content_tr = _get_work_content(work, tr=True)
         _work.compress_content = re.sub(r'[\s+]', '', _work.content)
-        _work.compress_content = re.sub(r'[\s+]', '', _work.content_tr)
+        _work.compress_content_tr = re.sub(r'[\s+]', '', _work.content_tr)
         _work.intro = work.intro.replace('\r\n\r\n', '\n')
         _work.intro_tr = work.intro_tr.replace('\r\n\r\n', '\n')
         _work.layout = work.layout
